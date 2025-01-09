f877395794a1bf2985e00f19cd7e784720a2d50e
mantra-gor <mantragor77@gmail.com>
2025-01-09 14:11:23 +0530

    auth page design done.

--- src/react/components/core/layout/AuthLayout.tsx
+++ src/react/components/core/layout/AuthLayout.tsx
@@ -3,20 +3,8 @@ import logo from "../../../assets/branding/logo-no-background.png";
 import SignIn from "../../../pages/SignIn";
 import SignUp from "../../../pages/SignUp";
 import accounting from "../../../assets/resources/accounting.mp4";
-// import animationData from "../../../assets/lottie/Meditation.json";
-// import Lottie from "react-lottie";
 
 function AuthLayout({ formType }: { formType: string }) {
-  // const defaultOptions = {
-  //   loop: true,
-  //   autoplay: true,
-  //   animationData: animationData,
-  //   rendererSettings: {
-  //     preserveAspectRatio: "xMidYMid slice",
-  //     clearCanvas: false, // Avoid potential canvas issues
-  //   },
-  // };
-
   return (
     <div className="w-screen h-screen overflow-hidden flex">
       <BackgroundGraphic1 position="bottom" />
@@ -25,7 +13,6 @@ function AuthLayout({ formType }: { formType: string }) {
         <div>{formType === "signin" ? <SignIn /> : <SignUp />}</div>
       </div>
       <div className="w-5/12 my-3 rounded-l-2xl overflow-hidden  flex justify-center items-center ">
-        {/* <Lottie options={defaultOptions} /> */}
         <div style={{ position: "relative", width: "100%", height: "auto" }}>
           <video
             src={accounting}
@@ -34,14 +21,17 @@ function AuthLayout({ formType }: { formType: string }) {
             loop
             className="w-full h-full relative"
           />
-          <div className="absolute top-0 left-0 w-full h-full z-10 bg-black bg-opacity-20" />
+          <div className="absolute top-0 left-0 w-full h-full z-10 bg-black bg-opacity-30" />
           <div className="absolute flex flex-col gap-4 bottom-[20%] left-[10%] z-20 text-5xl text-white font-semibold tracking-wider">
             <div>Make Your</div>
-            <div>Finances</div>
-            <div>Sane</div>
+            <div className="italic">Finances</div>
+            <div className="italic animate-bounce">Sane</div>
           </div>
         </div>
       </div>
+      <footer className="absolute left-0 bottom-0 w-7/12 text-center p-3 text-[#1b1b1b]">
+        © 2025 BillSane. All rights reserved.
+      </footer>
     </div>
   );
 }
